be7eca30c7421f63b20065a98126ed561ce9dce4
Jae Geun <jaegeunlee11@gmail.com>
2022-11-26 12:38:17 +0800

    Update format for old

--- practice1/calendar1.py
+++ practice1/calendar1.py
@@ -6,34 +6,33 @@
 from tkinter import *
 import calendar
 
-def showCalendar():
+
+def show_Calendar():
     gui = Tk()
     gui.config(background='blue')
     gui.title("JG Calendar")
     gui.geometry("600x600")
     year = int(year_field.get())
-    gui_content= calendar.calendar(year)
-    calYear = Label(gui, text = gui_content, font= "times 10 bold")
-    calYear.grid(row=5, column=1,padx=20)
+    gui_content = calendar.calendar(year)
+    calYear = Label(gui, text=gui_content, font="times 10 bold")
+    calYear.grid(row=5, column=1, padx=20)
     gui.mainloop()
 
 
-
-if __name__ == "__main__" :
+if __name__ == "__main__":
     new = Tk()
-    new.config(background = 'green')
+    new.config(background='green')
     new.title("Calender")
     new.geometry("250x140")
-    cal = Label(new, text="calendar", bg='grey',font="arial 28 bold")
-    year = Label(new, text = "Enter year", bg= "yellow")
-    year_field=Entry(new)
+    cal = Label(new, text="calendar", bg='grey', font="arial 28 bold")
+    year = Label(new, text="Enter year", bg="yellow")
+    year_field = Entry(new)
     button = Button(new, text="show Calendar",
-                    fg="red",bg="orange",command=showCalendar)
+                    fg="red", bg="orange", command=show_Calendar)
 
     cal.grid(row=1, column=1)
-    year.grid(row=2, column=1)
+    year.grid(row=2, column=1)  # todo
     year_field.grid(row=3, column=1)
     button.grid(row=4, column=1)
-
 
     new.mainloop()
